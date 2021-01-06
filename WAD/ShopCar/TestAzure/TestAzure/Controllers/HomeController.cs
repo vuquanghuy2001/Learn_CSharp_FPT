faa11a6b56f7123eb0d0f3cb1e22a9b8adbe8e66
vuquanghuy2001 <59812614+vuquanghuy2001@users.noreply.github.com>
2021-01-06 12:15:21 +0700

    update

---
+++ WAD/ShopCar/TestAzure/TestAzure/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using TestAzure.Models;
+
+namespace TestAzure.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+        //private readonly ILogger<HomeController> _logger;
+
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        //public IActionResult Privacy()
+        //{
+        //    return View();
+        //}
+
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
+    }
+}
