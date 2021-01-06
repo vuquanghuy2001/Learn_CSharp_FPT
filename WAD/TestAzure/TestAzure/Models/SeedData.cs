457e95ba5db95b315a978bf0b6a3efc7e116b6b8
vuquanghuy2001 <59812614+vuquanghuy2001@users.noreply.github.com>
2021-01-06 12:10:11 +0700

    update

---
+++ WAD/TestAzure/TestAzure/Models/SeedData.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestAzure.Models.DbContexts;
+
+namespace TestAzure.Models
+{
+    public static class SeedData
+    {
+        public static void EnsurePopulated (IApplicationBuilder app)
+        {
+            TAzureDbContext context = app.ApplicationServices
+                .CreateScope().ServiceProvider.GetRequiredService<TAzureDbContext>();
+
+            if (context.Database.GetPendingMigrations().Any())
+            {
+                context.Database.Migrate();
+            }
+
+            if (!context.Products.Any())
+            {
+                context.Products.AddRange(
+                    new Product
+                    {
+                        Name="Kayak", Description="A boat for one person",
+                        Category="Watersports", Price=275
+                    },
+                    new Product
+                    {
+                        Name = "Lifejacket",
+                        Description = "protective and fashionable",
+                        Category = "C",
+                        Price = 275
+                    },
+                    new Product
+                    {
+                        Name = "Kayak",
+                        Description = "A boat for one person",
+                        Category = "Watersports",
+                        Price = 275
+                    },
+                    new Product
+                    {
+                        Name = "Kayak",
+                        Description = "A boat for one person",
+                        Category = "Watersports",
+                        Price = 275
+                    },
+                    new Product
+                    {
+                        Name = "Kayak",
+                        Description = "A boat for one person",
+                        Category = "Watersports",
+                        Price = 275
+                    },
+                    new Product
+                    {
+                        Name = "Kayak",
+                        Description = "A boat for one person",
+                        Category = "Watersports",
+                        Price = 275
+                    },
+                    new Product
+                    {
+                        Name = "Kayak",
+                        Description = "A boat for one person",
+                        Category = "Watersports",
+                        Price = 275
+                    },
+                    new Product
+                    {
+                        Name = "Kayak",
+                        Description = "A boat for one person",
+                        Category = "Watersports",
+                        Price = 275
+                    },
+                    new Product
+                    {
+                        Name = "Kayak",
+                        Description = "A boat for one person",
+                        Category = "Watersports",
+                        Price = 275
+                    },
+                    new Product
+                    {
+                        Name = "Kayak",
+                        Description = "A boat for one person",
+                        Category = "Watersports",
+                        Price = 275
+                    }
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}
